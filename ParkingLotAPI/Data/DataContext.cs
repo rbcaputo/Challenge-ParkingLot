4eef13cadbbd11bf82df0845167040d87aaa505f
rbcaputo <rbcaputo@gmail.com>
2024-09-12 19:09:18 -0300

    fix: Fixed DataContext missing DBContextOptions<>

--- ParkingLotAPI/Data/DataContext.cs
+++ ParkingLotAPI/Data/DataContext.cs
@@ -3,8 +3,10 @@ using ParkingLotAPI.Models.Lot;
 
 namespace ParkingLotAPI.Data
 {
-	public class DataContext : DbContext
+	public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 	{
+		private readonly DbContextOptions<DataContext> _options = options;
+
 		public DbSet<VehicleModel> Vehicles { get; set; }
 		public DbSet<FareModel> Fares { get; set; }
 		public DbSet<ParkingModel> Parkings { get; set; }
@@ -22,6 +24,10 @@ namespace ParkingLotAPI.Data
 				.WithOne()
 				.HasForeignKey<ParkingModel>(p => p.FareId)
 				.OnDelete(DeleteBehavior.Restrict);
+
+			builder.Entity<FareModel>()
+				.Property(f => f.PricePerHour)
+				.HasColumnType("decimal(18, 2)");
 
 			base.OnModelCreating(builder);
 		}
